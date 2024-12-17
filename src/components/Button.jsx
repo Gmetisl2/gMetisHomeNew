a940a36e796df5bd505862ab03864916d299b7d9
Gmetisl2 <github@gmetis.io>
2024-12-17 02:02:46 +0100

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = ({ styles }) => {
+const Button = ({ styles, text, link }) => {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
-      Get Started
-    </button>
+    <a href={link} className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
+      {text}
+    </a>
   )
 }
 
