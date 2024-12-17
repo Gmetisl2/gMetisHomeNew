e337f99f85766f9f292032ba4a9c54f131c89c53
Gmetisl2 <github@gmetis.io>
2024-12-17 03:09:29 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ const Hero = () => {
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className='flex flex-row justify-between items-center w-full'>
-          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]'>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[42px] text-white ss:leading-[100.8px] leading-[60px]'>
             gMetis <br className='sm:block hidden'/> {" "}
             <span className='text-gradient'>Revolutionizing</span> {" "}
           </h1>
         </div>
-        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[42px] text-white ss:leading-[100px] leading-[60px] w-full'>
           Community Engagement with AI.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
