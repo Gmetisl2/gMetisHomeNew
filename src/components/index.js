74545e18cc8fe19b8a34c14fd07cfbfbc7e40f34
Gmetisl2 <github@gmetis.io>
2024-12-17 01:21:35 +0100

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,11 @@ import Business from './Business'
 import CardDeal from './CardDeal'
 import Clients from './Clients'
 import CTA from './CTA'
-import Feedback from './Feedback'
 import Footer from './Footer'
 import Hero from './Hero'
 import Navbar from './Navbar'
 import Stats from './Stats'
-import Testimonials from './Testimonials'
+
 
 export {
     Billing,
@@ -16,10 +15,8 @@ export {
     CardDeal,
     Clients,
     CTA,
-    Feedback,
     Footer,
     Hero,
     Navbar,
-    Stats,
-    Testimonials
+    Stats
 }
