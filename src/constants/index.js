19b2450935b5b9cfccaefa07720b969ff6d2a4cc
Gmetisl2 <github@gmetis.io>
2024-12-17 01:06:34 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -88,18 +88,18 @@ export const feedback = [
 export const stats = [
   {
     id: "stats-1",
-    title: "User Active",
-    value: "3800+",
+    title: "AI_Agent",
+    value: "",
   },
   {
     id: "stats-2",
-    title: "Trusted by Company",
-    value: "230+",
+    title: "MEME Token",
+    value: "",
   },
   {
     id: "stats-3",
-    title: "Transaction",
-    value: "$230M+",
+    title: "with Utility",
+    value: "",
   },
 ];
 
