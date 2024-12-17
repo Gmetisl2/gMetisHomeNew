f4d76eed10eebc3c12d5794f46ad7159645312c2
Gmetisl2 <github@gmetis.io>
2024-12-17 02:58:43 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -24,8 +24,10 @@ import twitter from "./twitter.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import telegram from './telegram.svg';
 
 export {
+  telegram,
   gmetis,
   airbnb,
   bill,
