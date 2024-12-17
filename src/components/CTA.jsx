18c4faa593296b26e4be2f4dad5d54c20d551b2a
Gmetisl2 <github@gmetis.io>
2024-12-17 02:29:03 +0100

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,13 +6,17 @@ const CTA = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
       <div className='flex-1 flex flex-col'>
-        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <h2 className={styles.heading2}>Discover the Power of gMetis</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Everything you need to accept card payments and grow your business anywhere on the planet.
+          Learn how gMetis leverages AI to create a dynamic and rewarding community. Dive into our comprehensive documentation to understand our tokenomics, reward system, and more.
         </p>
       </div>
       <div className={`${styles.flexCenter} sm:ml-10 ml-0`}>
-        <Button/>
+        <Button 
+          styles="mt-10" 
+          text="Read Documentation" 
+          link="https://docs.gmetis.io" 
+        />
       </div>
     </section>
   )
