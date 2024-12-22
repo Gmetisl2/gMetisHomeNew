8eba274194bd0d7df196d5d86766905fdbc27701
Gmetisl2 <github@gmetis.io>
2024-12-22 09:42:42 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,7 +25,7 @@ import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
 import telegram from './telegram.svg';
-import cvpbadge from ',/cvpbadge.svg';
+import cvpbadge from './cvpbadge.svg';
 
 export {
   cvpbadge
