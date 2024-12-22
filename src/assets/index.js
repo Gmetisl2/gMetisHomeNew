a07495f0ac87105dc002c60eccafcae1b7c3b4e0
Gmetisl2 <github@gmetis.io>
2024-12-22 09:41:12 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,8 +25,10 @@ import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
 import telegram from './telegram.svg';
+import cvpbadge from ',/cvpbadge.svg';
 
 export {
+  cvpbadge
   telegram,
   gmetis,
   airbnb,
